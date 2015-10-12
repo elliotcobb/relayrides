c6270896d05896c21ebb189f60608c489603dee4
Elliot Cobb <elliot@liveclicker.com>
2015-10-12 00:16:15 -0400

    experimenting with accordion

--- web/js/index.js
+++ web/js/index.js
@@ -1,12 +1,46 @@
 /**
  * Created by elliotcobb on 10/11/15.
  */
+
 $(document).ready( function() {
-    searchFor("default");
+    searchFor("LAX", "04/20/2016", "04/22/2016");
 });
 
-function searchFor( query ) {
-    $.get( "/web/search.php?q=" + query, function( data ) {
-    console.log('search returned: ' + data);
+$('.panel.car-result').click( function() {
+   //$(this).find('.panel-body').
+});
+
+function searchFor( query, date1, date2 ) {
+    var target_url = "/web/search.php?dest=" + query + "&date1=" + date1 + "&date2=" + date2;
+    $.get( target_url, function( data ) {
+        console.log("search result: ");
+        var search_results = JSON.parse( data );
+        if (search_results['Result']['CarResult'] === undefined) {
+            console.log('errors abound');
+            console.log(search_results);
+        } else {
+            console.log('no errors here');
+            buildSearchResults( search_results );
+        }
     });
+}
+
+function buildSearchResults( search_results ) {
+    console.log(search_results);
+    //var car_meta_data = search_results['']
+    var car_array = search_results['Result']['CarResult'];
+    var car_array_length = car_array.length;
+    var result_div;
+
+    for (var i = 0; i < car_array_length; i++) {
+        result_div = $('.car-result.clone').clone();
+        result_div.find('.panel-title .title-link').text(i + '. ' + car_array[i]['DailyRate'] + ' Airport: ' + car_array[i]['PickupAirport']);
+        result_div.find('.panel-body').text('Date: ' + car_array[i]['PickupDay']);
+        result_div.find('.panel-body .make-reservation').attr('href', car_array[i]['DeepLink']);
+        // fill in car data2
+        result_div.removeClass('clone');
+        result_div.find('.panel-body').append("\nthis is elem: " + i);
+        $('body').append(result_div);
+        //console.log(result_div);
+    }
 }
--- web/search.php
+++ web/search.php
@@ -6,9 +6,35 @@
  * Time: 6:07 PM
  */
 
+ini_set('display_errors',1);
 
-if (isset($_GET['q'])) {
-    echo "your search is: " . $_GET['q'];
+const API_KEY = "rb3cgkm76er2jbcqr9ckkpma";
+
+function quickCURL( $url ) {
+
+    $ch = curl_init();
+    $timeout = 5;
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
 }
 
+if (isset($_GET['dest']) && isset($_GET['date1']) && isset($_GET['date2'])) {
+
+    $curl_url = "http://api.hotwire.com/v1/search/car?apikey=" . API_KEY .
+                "&dest=" . $_GET['dest'] .
+                "&startdate=" . $_GET['date1'] .
+                "&enddate=" . $_GET['date2'] .
+                "&pickuptime=" . "9:00" .
+                "&dropofftime=" . "12:00";
+
+//    echo $curl_url;
+    $search_result = quickCURL($curl_url);
+    $search_result_xml = simplexml_load_string($search_result);
+    $search_result_json = json_encode($search_result_xml);
+    echo $search_result_json;
+}
 ?>
